dd76a863d1dd8cfec95b8869a0877375b3362cc9
James Mendel <whoopseez@gmail.com>
2020-08-03 13:42:01 -0500

    chrome bugfix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Injects ku-rmp.js into DOM to have direct access to document elements
 (function kuRMP() {
-    var manifest = browser.runtime.getManifest()
+    var manifest = chrome.runtime.getManifest()
     console.log(`%c ${manifest.name}`, 'font-weight: bold; font-size: 50px;color: #e8000d; text-shadow: 3px 3px 0 white , 6px 6px 0 #0051ba');
     console.log(`\tLoaded Extention: ${manifest.name} ~ Version ${manifest.version}\n\tBy ${manifest.author} - ${manifest.homepage_url}`);
 
